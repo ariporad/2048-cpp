beb38582b839f1b049b4e69190cee7f47c136161
Ari Porad <ari@ariporad.com>
2019-02-24 10:03:14 -0700

    Initial Commit

---
+++ C++ 2048/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  C++ 2048
+//
+//  Created by Ari Porad on 2019-02-24.
+//  Copyright © 2019 Chocoholic Studios. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
